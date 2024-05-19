16a8fb7eebe011aa47982016b98c02837ed710f1
kauã oraboni <70423924+Orabonii@users.noreply.github.com>
2024-05-19 18:02:17 -0300

    Add files via upload

---
+++ projeto06/src/App.jsx
@@ -0,0 +1,21 @@
+import React from 'react'; 
+
+import GaleriaDeImagens from './Components/Projeto06/Projeto06'; 
+import './App.css'
+  
+
+function App() { 
+
+  return ( 
+
+    <div className="App"> 
+
+      <GaleriaDeImagens/> 
+
+    </div> 
+
+  ); 
+
+} 
+
+export default App; 
